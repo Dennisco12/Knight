215979294830706bc470aa4b12289630576d52d5
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-12-02 01:59:58 -0800

    updated

--- to-do_app/api/web_dynamic/static/styles/script.js
+++ to-do_app/api/web_dynamic/static/styles/script.js
@@ -3,7 +3,8 @@ $('document').ready(function () {
 	const api = 'http://' + window.location.hostname;
 	$('button.add').click( () => {
 		$('div.form').append('<input class="title"></input><input class="description"></input>')
-}
+	});
+});
 
 $.ajax({
 	url: api + ':5000/api/tasks',
