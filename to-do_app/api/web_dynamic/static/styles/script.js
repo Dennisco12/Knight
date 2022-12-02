95a62c4f19a4e83bd0f1e195049c3ddb3151416e
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-12-02 10:23:22 -0800

    updated

--- to-do_app/api/web_dynamic/static/styles/script.js
+++ to-do_app/api/web_dynamic/static/styles/script.js
@@ -2,14 +2,22 @@
 $('document').ready(function () {
 	const api = 'http://' + window.location.hostname;
 	$('button.add').click( () => {
-		$('div.form').append('<input class="title"></input><input class="description"></input>')
+		$('div.form').append('<p>Title: <input class="title"></p><p>Description: </input><input class="description"></input><button class="submit"></button>')
+	});
+	const title = $('div.form input.title').val
+	const description = $('div.form input.description').val
+	$('div.form button.submit').click( () => {
+		const myDict = {title: decription};
+		function addTask(myDict)
 	});
 });
 
-$.ajax({
-	url: api + ':5000/api/tasks',
-	type: 'POST',
-	data: {},
-	contentType: 'application/json',
-	dataType: 'json',
+$('div.form button.submit').click(addTask(myDict) {
+	$.ajax({
+		url: api + ':5000/api/tasks',
+		type: 'POST',
+		data: JSON.stringify(myDict),
+		contentType: 'application/json',
+		dataType: 'json',
+	});
 });
