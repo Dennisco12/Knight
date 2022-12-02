4248e9ab76780b99148dc50da6c5de73f7a7c6e1
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-12-02 10:38:07 -0800

    update

--- to-do_app/api/web_dynamic/static/styles/script.js
+++ to-do_app/api/web_dynamic/static/styles/script.js
@@ -2,9 +2,7 @@
 $('document').ready(function () {
 	const api = 'http://' + window.location.hostname;
 	$('button.add').click( () => {
-		$('div.form').append(`<p>Title: <input class="title"></p>
-			<p>Description: </input><input class="description"></input></p>
-			<button class="submit"></button>`)
+		$('div.form').append('<p>Title: <input class="title"></p><p>Description: </input><input class="description"></input></p><button class="submit"></button>')
 	});
 	const title = $('div.form input.title').val
 	const description = $('div.form input.description').val
